7b80d196f8295fd8268d6f8924607117cf4ff4e4
Marc F <marcfogleman@gmail.com>
2016-12-02 21:35:08 -0500

    routing

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var pg = require('pg');
 var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(''));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname + '/index.html'));
 });
